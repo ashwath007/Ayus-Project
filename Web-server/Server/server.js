5e3eb0858b18531b85e567a35456ee08754b45f2
vicky <srivicky2000@gmail.com>
2021-05-26 14:56:59 +0530

    mongodb added

--- Web-server/Server/server.js
+++ Web-server/Server/server.js
@@ -1,35 +1,36 @@
+require("dotenv").config();
 const express = require('express');
 const app = express();
-var mysql = require('mysql');
-
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const chalk = require('chalk');
+const mongoose = require('mongoose');
 
 const port = process.env.PORT || 8000;
 
 // Data Base Setup
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'MYAYUS'
+// DATABASE
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(() => {
+    console.log(chalk.red("Database Connected"));
 });
 
 
+//Middlewares
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
 
 
-app.get("/", (req, res) => {
-    res.send("Good");
-    connection.connect(function(err) {
-        if (err) throw err;
-        console.log("Connected!");
-        var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
-        connection.query(sql, function(err, result) {
-            if (err) throw err;
-            console.log("Table created");
-        });
-    });
+const userRoute = require("./routes/user");
 
-});
+
+app.use("/api", userRoute);
 
 app.listen(port, () => {
     console.log(chalk.green(`Server started at ${port}`));
